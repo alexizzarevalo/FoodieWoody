b2690061de7e44d8e199e625d5ae3fe96e92f847
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-08-22 16:13:37 -0600

    Se agrego pantalla confirmación de pedido

---
+++ src/components/orderConfirmation/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Text, View} from 'react-native';
+
+export default function CartNavigation() {
+    return(
+      <View>
+        <Text>Hello order screen</Text>
+      </View>
+    );
+}
--- src/navigation/cart_Navigation/index.tsx
+++ src/navigation/cart_Navigation/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Text, View} from 'react-native';
 import {CartStackParamList} from '../types';
-
+import OrderConfirmation from '../../components/orderConfirmation';
 const Stack = createStackNavigator<CartStackParamList>();
 
 export default function CartNavigation() {
   return (
     <Stack.Navigator>
+      <Stack.Screen name={'Cart2'} component={OrderConfirmation} />
       <Stack.Screen name={'Cart'} component={ExampleCart} />
       {/*Agregar aqui las pantallas del carrito.
           La que está primero es la que se muestra por default*/}
