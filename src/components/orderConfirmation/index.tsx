2a53391dbb48545a9662a80bee3c85afbb937ddc
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-08-24 20:02:51 -0600

    Picker para el metodo de pago

--- src/components/orderConfirmation/index.tsx
+++ src/components/orderConfirmation/index.tsx
@@ -11,6 +11,7 @@ import {StackHeaderOptions} from "@react-navigation/stack/lib/typescript/src/typ
 
 export default function orderConfirmation({route, navigation}:StackHeaderProps) {
   const [selectedValue, setSelectedValue] = useState("");
+  const [selectedCard, setSelectedCard] = useState("");
   const [costumerInformation, setCostumerInformation] = useState(
       {
         name: 'Cristian Castellanos',
@@ -20,6 +21,10 @@ export default function orderConfirmation({route, navigation}:StackHeaderProps) 
           {key: 2,
             direccion: "32 calle 21-30, colonia Santa Elisa, zona 12"}
         ],
+        tarjetas:[
+          {key:3,numero:'6434'},
+          {key:4,numero:'4515'},
+        ]
       }
   );
   const {total} = route.params
@@ -44,6 +49,18 @@ export default function orderConfirmation({route, navigation}:StackHeaderProps) 
               {costumerInformation.direcciones.map((d)=>{
                 return(
                   <Picker.Item label={d.direccion} value={d.direccion} key={d.key} />
+                );
+              })}
+            </Picker>
+            <Text style={styles.label} >Metodo de pago</Text>
+            <Picker
+              selectedValue={selectedCard}
+              style={styles.picker}
+              onValueChange={(itemValue, itemIndex) => setSelectedCard(itemValue.toString())}
+            >
+              {costumerInformation.tarjetas.map((t)=>{
+                return(
+                  <Picker.Item label={"*******"+t.numero} value={t.key} key={t.key} />
                 );
               })}
             </Picker>
