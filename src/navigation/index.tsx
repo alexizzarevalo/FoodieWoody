0579070bcb1c2f41d28de03ba808ed44e4edc9b2
alexizzarevalo <dalexis.da@gmail.com>
2020-08-26 10:53:21 -0600

    Se agrega un hook para obtener el usuario actual. Se agrega proteccion de rutas en DrawerNavigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import CartNavigation from './cart_Navigation';
-import {DrawerParamList} from './types';
+import { DrawerParamList } from './types';
 import LoginScreen from "../components/loginScreen";
+import useUser from '../hooks/useUser';
 
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
 export default function Navigation() {
+  const { isLogged } = useUser();
+
   return (
     <NavigationContainer>
       <Drawer.Navigator drawerType={'back'}>
-        <Drawer.Screen name={'Login'} component={LoginScreen} />
-        <Drawer.Screen name={'Cart'} component={CartNavigation} />
-        {/*Agregar aqui las demas pantallas*/}
+        {
+          !isLogged ?
+            <Drawer.Screen options={{ title: 'Iniciar sesión' }} name={'Login'} component={LoginScreen} />
+            :
+            <>
+              <Drawer.Screen name={'Cart'} component={CartNavigation} />
+              {/*Agregar aqui las demas pantallas*/}
+            </>
+        }
       </Drawer.Navigator>
     </NavigationContainer>
   );
