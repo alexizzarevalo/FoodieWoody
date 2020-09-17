c3d1c1a1afb268af56f1bdfa5430268d816d40e7
Max Florian <maxgt734@gmail.com>
2020-09-17 11:22:10 -0600

    Se realizó test de text inputs a pantalla 'CrearReceta'

--- src/components/crearReceta/index.tsx
+++ src/components/crearReceta/index.tsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
   StatusBar,
+  View,
+  TextInput
 } from 'react-native';
 
 
 declare const global: {HermesInternal: null | {}};
 
-const CrearReceta = () => {
+export default function CrearReceta(){
+  const [text, setText] = useState('');
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Text> Crea una receta </Text>
+        <View style={styles.container}>
+            <Text> Crea una receta </Text>
+            <View style={styles.continput}>
+                <View style={styles.top}>
+                    <Text> Nombre: </Text>
+                </View>
+                <View style={styles.bottom}>
+                <TextInput
+                    style={styles.texti}
+                    underlineColorAndroid = "transparent"
+                    placeholder=""
+                    onChangeText={text => setText(text)}
+                    defaultValue=""
+                />
+                </View>
+            </View>
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-
+    container : {
+        backgroundColor: '#f87c09'
+    },
+    continput : {
+        marginTop : 10
+    },
+    top : {
+        flex : 1,
+        justifyContent : 'flex-start',
+        alignItems : 'flex-start'
+    },
+    bottom : {
+        flex : 2,
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    texti : {
+        height : 30,
+        width : '60%',
+        backgroundColor : 'white',
+        borderColor : 'purple',
+        borderWidth : 1,
+        borderRadius: 5
+    }
 });
 
-export default CrearReceta;
