0e9a5de5a279bae89750b979b9f2742c196d37f3
Max Florian <maxgt734@gmail.com>
2020-09-21 17:38:34 -0600

    Se realizó test final a interfaz gráfica a pantalla 'CrearReceta'

--- src/components/crearReceta/index.tsx
+++ src/components/crearReceta/index.tsx
@@ -5,120 +5,151 @@ import {
   Text,
   StatusBar,
   View,
-  TextInput
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 
 declare const global: {HermesInternal: null | {}};
 
 export default function CrearReceta(){
-  const [text, setText] = useState('');
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.container}>
-            <Text> Crea una receta </Text>
-            <View style={styles.continput}>
-                <View style={styles.top}>
-                    <Text> Nombre: </Text>
-                </View>
-                <View style={styles.bottom}>
-                <TextInput
-                    style={styles.texti}
-                    underlineColorAndroid = "transparent"
-                    placeholder=""
-                    onChangeText={text => setText(text)}
-                    defaultValue=""
-                />
-                </View>
-            </View>
-            <View style={styles.continput}>
-                <View style={styles.top}>
-                    <Text> Descripción: </Text>
-                </View>
-                <View style={styles.bottom}>
-                <TextInput
-                    style={styles.texti}
-                    underlineColorAndroid = "transparent"
-                    placeholder=""
-                    onChangeText={text => setText(text)}
-                    defaultValue=""
-                />
-                </View>
-            </View>
-            <View style={styles.continput}>
-                <View style={styles.top}>
-                    <Text> Ingredientes: </Text>
-                </View>
-                <View style={styles.bottom}>
-                <TextInput
-                    style={styles.texti}
-                    underlineColorAndroid = "transparent"
-                    placeholder=""
-                    onChangeText={text => setText(text)}
-                    defaultValue=""
-                />
-                </View>
-            </View>
-            <View style={styles.continput}>
-                <View style={styles.top}>
-                    <Text> Tiempo de preparación: </Text>
-                </View>
-                <View style={styles.bottom}>
-                <TextInput
-                    style={styles.texti}
-                    underlineColorAndroid = "transparent"
-                    placeholder=""
-                    onChangeText={text => setText(text)}
-                    defaultValue=""
-                />
-                </View>
-            </View>
-            <View style={styles.continput}>
-                <View style={styles.top}>
-                    <Text> Costo total: </Text>
-                </View>
-                <View style={styles.bottom}>
-                <TextInput
-                    style={styles.texti}
-                    underlineColorAndroid = "transparent"
-                    placeholder=""
-                    onChangeText={text => setText(text)}
-                    defaultValue=""
-                />
-                </View>
-            </View>
-        </View>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-    container : {
-        backgroundColor: '#f87c09'
-    },
-    continput : {
-        marginTop : 10
-    },
-    top : {
-        flex : 1,
-        justifyContent : 'flex-start',
-        alignItems : 'flex-start'
-    },
-    bottom : {
-        flex : 2,
-        justifyContent : 'center',
-        alignItems : 'center'
-    },
-    texti : {
-        height : 30,
-        width : '60%',
-        backgroundColor : 'white',
-        borderColor : 'purple',
-        borderWidth : 1,
-        borderRadius: 5
-    }
-});
+    const [text, setText] = useState('');
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+          <View style={styles.container}>
+              <Text style={styles.titulo}> Crea una receta </Text>
+  
+                      <Text style={styles.textos}> Nombre: </Text>
+                  
+                  
+                  <TextInput
+                      style={styles.texti}
+                      underlineColorAndroid = "transparent"
+                      placeholder=""
+                      onChangeText={text => setText(text)}
+                      defaultValue=""
+                  />
+                  
+             
+              <View style={styles.separador}></View>
+              
+                  
+                      <Text> Descripción: </Text>
+                  
+                  
+                  <TextInput
+                      style={styles.textig}
+                      underlineColorAndroid = "transparent"
+                      placeholder=""
+                      onChangeText={text => setText(text)}
+                      defaultValue=""
+                  />
+              
+              
+              <View style={styles.separador}></View>
+              
+                  
+                      <Text> Ingredientes: </Text>
+                 
+                  
+                  <TextInput
+                      style={styles.textig}
+                      underlineColorAndroid = "transparent"
+                      placeholder=""
+                      onChangeText={text => setText(text)}
+                      defaultValue=""
+                  />
+                  
+              
+              <View style={styles.separador}></View>
+              
+                  
+                      <Text> Tiempo de preparación: </Text>
+                  
+                  
+                  <TextInput
+                      style={styles.texti}
+                      underlineColorAndroid = "transparent"
+                      placeholder=""
+                      onChangeText={text => setText(text)}
+                      defaultValue=""
+                  />
+                  
+              
+              <View style={styles.separador}></View>
+              
+                  
+                      <Text> Costo total: </Text>
+                  
+                 
+                  <TextInput
+                      style={styles.texti}
+                      underlineColorAndroid = "transparent"
+                      placeholder=""
+                      onChangeText={text => setText(text)}
+                      defaultValue=""
+                  />
+                 
+              
+              <TouchableOpacity
+                style={styles.boton}
+                
+              >
+                <Text>Crear</Text>
+              </TouchableOpacity>
+          </View>
+      </>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+      container : {
+          flex : 1,
+          flexDirection : 'column',
+          backgroundColor: '#f87c09'
+      },
+      textos : {
+        marginBottom : 5
+      },
+      titulo : {
+        textAlign: 'center',
+        marginTop: 20,
+        marginBottom : 20,
+        fontSize: 24
+      },
+      texti : {
+        marginLeft: 40,
+          height : 30,
+          width : '80%',
+          backgroundColor : 'white',
+          borderColor : 'purple',
+          borderWidth : 1,
+          borderRadius: 5
+      },
+      textig : {
+        marginLeft: 40,
+          height : 110,
+          width : '80%',
+          backgroundColor : 'white',
+          borderColor : 'purple',
+          borderWidth : 1,
+          borderRadius: 5
+      },
+      boton : {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10,
+        width: '80%',
+        justifyContent: 'flex-end',
+        alignSelf: 'center',
+        marginTop : 20
+      },
+      separador : {
+        borderBottomColor: 'gray',
+        borderBottomWidth: 1,
+        marginTop : 10,
+        marginBottom : 10
+      }
+  });
 
