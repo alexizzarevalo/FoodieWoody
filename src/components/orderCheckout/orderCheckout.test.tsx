2897ef64332180c1b75dec9fe2763dac844b5607
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-09-21 20:51:33 -0600

    Se agrego el test de navegacion a la pantalla de confirmacion, sin act()

--- src/components/orderCheckout/orderCheckout.test.tsx
+++ src/components/orderCheckout/orderCheckout.test.tsx
@@ -1,7 +1,7 @@
 //import '../__mocks__/firebase-auth-mock';
 
 import React from 'react';
-import renderer from 'react-test-renderer'
+import renderer, {act} from 'react-test-renderer';
 import OrderCheckout from './orderCheckout.component';
 
 // redux
@@ -9,6 +9,9 @@ import {Provider} from 'react-redux';
 import {GlobalState} from '../../storage/global-state.interface';
 import configureStore from 'redux-mock-store';
 import {store} from '../../storage';
+import Template from './orderCheckout.view';
+import {fireEvent, render} from '@testing-library/react-native';
+import CarItem from './CarItem/CarItem.component';
 const mockStore = configureStore([]);
 
 describe('Order confirmation screen component', () => {
@@ -22,16 +25,42 @@ describe('Order confirmation screen component', () => {
     search: '',
   }
 
+  const cart:any = []
+  const itemDetails:any = []
+  const total = 56
+  const confirmacion = () => { }
+
   //Configure redux
   beforeEach(() => {
     store = mockStore(initialState);
   });
 
-  test('renders', () => {
+  test('Should renders hole component', () => {
     renderer.create(
       <Provider store={store}>
         <OrderCheckout navigation={navigation} route={route}/>
       </Provider>)
+  })
+  test('Should renders view with no empty car', () => {
+    cart.push({
+      receta_id: 'idfalso',
+      cantidad: 3
+    })
+    renderer.create(
+      <Provider store={store}>
+        <Template cart={cart} itemDetails={itemDetails} total={total} confirmacion={confirmacion}/>
+      </Provider>
+    )
+  })
+
+  test('Should navigate to confirmation screen', () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Template cart={cart} itemDetails={itemDetails} total={total} confirmacion={confirmacion}/>
+      </Provider>
+    )
+    const confirmationbtn = getByTestId('confirmationbtn');
+    fireEvent.press(confirmationbtn)
   })
 })
 
