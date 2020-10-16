68c273465a435663879755cdc9d2b067bf2afcf8
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-10-16 13:58:38 -0600

    Se implementan el codigo necesario para ejecutar correctamente

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,23 +1,31 @@
-echo "List files in APPCENTER_OUTPUT_DIRECTORY"
+# exit when any command fails
+set -e
+
+echo ">List files in APPCENTER_OUTPUT_DIRECTORY"
 ls $APPCENTER_OUTPUT_DIRECTORY
+echo ">List files in Actual directory ."
+ls .
 
 # Verify if application build works
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ];
 then
-	echo "Starting building UI test"
-	echo "Compiled project to run UI test"
+	echo ">Starting building UI test"
+	# echo ">Compiled project to run UI test"
 	# Start metro server npm run start
 	# npm install -g appcenter-cli
 	npm run start & # Avoid blocking terminal
-	gem install xamarin-test-cloud
-	echo "Running test in App Center Test"
-	# App does not exist at path: /Users/runner/work/1/a/build/app/build/outputs/apk/app-debug.apk 
-	# App does not exist at path: /Users/runner/work/1/a/build/android/app/build/outputs/apk/*.apk	
-	# App does not exist at path: /Users/runner/work/1/a/build/app-debug.apk
+	# echo "Global gem install"
+	# gem install xamarin-test-cloud
+	echo ">Bundle gem install"
+	bundle install
+	echo ">Building test server"
 	APPPATH=$APPCENTER_OUTPUT_DIRECTORY/app-release.apk 
-	PROJECTDIR=$APPCENTER_OUTPUT_DIRECTORY/features
-	appcenter test run calabash --app "foodiewoody/Foodie-Woody" --devices 5f746b78 --app-path $APPPATH --test-series "master" --locale "en_US" --project-dir $PROJECTDIR --token $API_TOKEN --async	
-	echo "Post build end test sended"
+	bundle exec calabash-android build $APPPATH
+	echo ">Running test in App Center Test"
+	bundle exec appcenter test run calabash --app "foodiewoody/Foodie-Woody" --devices 5f746b78 --app-path $APPPATH --test-series "master" --locale "en_US" --project-dir ./features --token $API_TOKEN --async
+	killall node
+	echo ">Post build end test sended"
+	exit 0
 else
-	echo "No test createded because build fails"
-fi
+	echo ">No test createded because build fails"
+fi	
