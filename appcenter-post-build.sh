30cfdfcf8cea09cfabcef7026dee3ee7e897d40d
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-10-17 01:06:34 -0600

    Se agregan login usando USER_TOKEN

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,10 +1,10 @@
 # exit when any command fails
 set -e
 
-echo ">List files in APPCENTER_OUTPUT_DIRECTORY"
-ls $APPCENTER_OUTPUT_DIRECTORY
-echo ">List files in Actual directory ."
-ls .
+# echo ">List files in APPCENTER_OUTPUT_DIRECTORY"
+# ls $APPCENTER_OUTPUT_DIRECTORY
+# echo ">List files in Actual directory ."
+# ls .
 
 # Verify if application build works
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ];
@@ -21,11 +21,13 @@ then
 	echo ">Building test server"
 	APPPATH=$APPCENTER_OUTPUT_DIRECTORY/app-release.apk 
 	bundle exec calabash-android build $APPPATH
+	echo ">Login into appcetner using User Token"
+	appcenter login --token $USER_TOKEN
 	echo ">Running test in App Center Test"
 	bundle exec appcenter test run calabash --app "foodiewoody/Foodie-Woody" --devices 5f746b78 --app-path $APPPATH --test-series "master" --locale "en_US" --project-dir ./features --token $API_TOKEN --async
-	killall node
+	kill -9
 	echo ">Post build end test sended"
-	exit 0
+	# exit 0
 else
 	echo ">No test createded because build fails"
 fi	
