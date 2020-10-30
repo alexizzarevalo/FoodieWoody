6e60f436e1ef84320ddd5e7fc5e984aa95ae9f7c
Max <maxgt734@gmail.com>
2020-10-29 21:23:09 -0600

    Se realizó estructura inciai de pantalla VerModificarPerfil

---
+++ src/components/VerModificarPerfil/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Text, View, StyleSheet} from 'react-native'
+
+function VerModificarPerfil(props: any){
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.top}>
+                <Text> {props.data.tipo} </Text>
+            </View>
+            <View style={styles.bottom}>
+                <Text> {props.data.contenido} </Text>
+            </View>
+        </View>
+    )
+}
+
+export default VerModificarPerfil
