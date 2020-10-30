06b17a6413ff053f3d763012962076909758c021
Max <maxgt734@gmail.com>
2020-10-29 21:32:37 -0600

    Se agrego estilo a los elementos de la flatlist en pantalla VerModificarPerfil

--- src/components/VerModificarPerfil/Element.tsx
+++ src/components/VerModificarPerfil/Element.tsx
@@ -1,18 +1,48 @@
-import React from 'react'
-import {Text, View, StyleSheet} from 'react-native'
+import React, { useState } from 'react'
+import {Text, View, StyleSheet, TextInput} from 'react-native'
 
 function Element(props: any){
-
+    const [text, setText] = useState('');
     return(
         <View style={styles.container}>
             <View style={styles.top}>
                 <Text> {props.data.tipo} </Text>
             </View>
             <View style={styles.bottom}>
-                <Text> {props.data.contenido} </Text>
+            <TextInput
+                style={styles.texti}
+                underlineColorAndroid = "transparent"
+                placeholder=""
+                onChangeText={text => setText(text)}
+                defaultValue={props.data.contenido}
+            />
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container : {
+        marginTop : 10
+    },
+    top : {
+        flex : 1,
+        justifyContent : 'flex-start',
+        alignItems : 'flex-start'
+    },
+    bottom : {
+        flex : 2,
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    texti : {
+        height : 30,
+        width : '60%',
+        backgroundColor : 'white',
+        borderColor : 'purple',
+        borderWidth : 1,
+        borderRadius: 5
+    }
+  });
 
 export default Element
